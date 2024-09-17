5535da9f4db3480423f492f2258647f2b36559bd
tc252 <contato.tereza25@gmail.com>
2024-09-17 00:06:01 +0000

    atualizando código

--- tiposPrimitivos.js
+++ tiposPrimitivos.js
@@ -1,1 +1,9 @@
+/*const ola = "HELLO WORLD" //declarar a variável
+ola = 123               //reatribuir a variável
+console.log(ola)*/
 
+const num1 = 10;
+const num2 = 20;
+const calc = num1 / num2
+
+console.log(soma)
